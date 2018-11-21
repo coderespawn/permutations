76a04be47276e3f2710ec031491fb439b7247d90
Ali Akbar <ali.akbar@coderespawn.com>
2018-11-21 22:48:30 +0530

    Runs permutations on the outgoing nodes after a combination of door list

--- Program.cs
+++ Program.cs
@@ -32,23 +32,30 @@ namespace Combinator
             TestSystem();
         }
 
+        static void PrintSelectionConfig(int[] Doors, string[] Nodes)
+        {
+            for (int i = 0; i < Doors.Length; i++)
+            {
+                Console.Write("{0}[{1}] ", Nodes[i], Doors[i]);
+            }
+            Console.WriteLine();
+        }
+
         static void TestSystem()
         {
             var DoorIndices = new int[] { 0, 1, 2, 3, 4 };
-            var Outgoing = new string[] { "R", "C" };
+            var Outgoing = new string[] { "R", "R", "C" };
 
             var combinator = new Combination<int>(DoorIndices, Outgoing.Length);
             while (combinator.CanRun)
             {
-                int[] Selection = combinator.Execute();
-                PrintResult(Selection);
+                int[] selection = combinator.Execute();
 
                 var permutation = new Permutation<string>(Outgoing);
                 while (permutation.CanPermute)
                 {
                     permutation.Permutate();
-                    PrintIndent(1);
-                    PrintResult(permutation.Data);
+                    PrintSelectionConfig(selection, permutation.Data);
                 }
 
                 Console.WriteLine();
